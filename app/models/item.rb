367e79932a265fa2c4e52ffc69b3f29db12c790f
jeferbc <jeffe.bernal@gmail.com>
2017-04-04 15:34:13 -0500

    items controller, model and testing

---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  done       :boolean
+#  todo_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Item < ApplicationRecord
+  belongs_to :todo
+  validates :name, presence: true
+end
